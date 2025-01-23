28b3b829bbfe850756d49d076fe0550a44c733b6
Bruni <bruniely.marchioro@zup.com.br>
2025-01-23 15:21:25 -0300

    Adiciona busca de produtos por nome (case insensitive) na classe ProductRepository

--- src/main/java/com/example/retornosAPI/repositories/ProductRepository.java
+++ src/main/java/com/example/retornosAPI/repositories/ProductRepository.java
@@ -6,6 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
-    // Metodo para buscar produtos pelo nome (case insensitive)
+
+    // Método para buscar produtos pelo nome (case insensitive)
     List<ProductEntity> findByNameContainingIgnoreCase(String name);
+
+    // Outros métodos personalizados que você pode precisar
+    List<ProductEntity> findByCategory(String category);
 }
