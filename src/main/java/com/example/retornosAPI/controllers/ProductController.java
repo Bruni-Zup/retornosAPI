e929288e2ba746c1196110527ef4f4f909dc5b02
Bruni <bruniely.marchioro@zup.com.br>
2025-01-23 15:48:59 -0300

    Adiciona validação de entrada, tratamento de exceções e melhoria na resposta de erros na classe ProductController

--- src/main/java/com/example/retornosAPI/controllers/ProductController.java
+++ src/main/java/com/example/retornosAPI/controllers/ProductController.java
@@ -2,9 +2,13 @@ package com.example.retornosAPI.controllers;
 
 import com.example.retornosAPI.models.Product;
 import com.example.retornosAPI.services.ProductService;
+import com.example.retornosAPI.validator.ProductValidator;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -12,24 +16,44 @@ import java.util.List;
 public class ProductController {
 
     private final ProductService service;
+    private final ProductValidator productValidator;
 
-    public ProductController(ProductService service) {
+    public ProductController(ProductService service, ProductValidator productValidator) {
         this.service = service;
+        this.productValidator = productValidator;
     }
 
     @PostMapping
-    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+    public ResponseEntity<?> createProduct(@RequestBody @Valid Product product, BindingResult result) {
+        // Validando o produto
+        productValidator.validate(product, result);
+
+        // Se houver erros de validação, retornar uma resposta com os erros
+        if (result.hasErrors()) {
+            StringBuilder errors = new StringBuilder();
+            for (ObjectError error : result.getAllErrors()) {
+                errors.append(error.getDefaultMessage()).append(" ");
+            }
+            return ResponseEntity.badRequest().body(errors.toString());
+        }
+
         return ResponseEntity.ok(service.createProduct(product));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Product> getProductById(@PathVariable Long id) {
-        return ResponseEntity.ok(service.getProductById(id));
+        Product product = service.getProductById(id);
+        if (product != null) {
+            return ResponseEntity.ok(product);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @GetMapping
     public ResponseEntity<List<Product>> getAllProducts() {
-        return ResponseEntity.ok(service.getAllProducts());
+        List<Product> products = service.getAllProducts();
+        return ResponseEntity.ok(products);
     }
 
     @DeleteMapping("/{id}")
